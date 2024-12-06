9183927c893448fbcc859341ddc5c6dd89ca4469
AmanSood <you@example.com>
2024-12-06 19:34:39 +0530

    Initial commit

--- application.py
+++ application.py
@@ -7,8 +7,8 @@ import json
 app = Flask(__name__)
 
 # Delhivery API details
-DELHIVERY_API_URL = "https://track.delhivery.com/api/v1/packages/json/"
-DELHIVERY_API_KEY = "5a7b27e8d938067996dbacce89a50cf909598111"
+
+DELHIVERY_API_KEY = "a4d484e7d39015a655fd6b3c6c10152adf7a49c5"
 
 # WooCommerce API credentials
 WOOCOMMERCE_URL = "https://figureshub.in/wp-json/wc/v3"
@@ -42,7 +42,7 @@ def check_woo():
                     awb_number = awb_response.get('awb_number')
                     if awb_number:
                         # Prepare order details with AWB number and tracking URL
-                        tracking_url = f"https://track.delhivery.com/api/v1/packages/json/?waybill={awb_number}"
+                        tracking_url = f"https://track.delhivery.com/api/v1/packages/json/?waybill={order_id}"
                         json_data = {
                             "order_id": order_id,
                             "status": order_status,
